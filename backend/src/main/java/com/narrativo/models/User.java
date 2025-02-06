726ddf93e8a354f9bf9650fb06d1ead3678a5c51
Alifiyah Shahid <shahid.alifiyah@gmail.com>
2025-02-06 10:24:30 -0500

    Initial Project setup

---
+++ backend/src/main/java/com/narrativo/models/User.java
@@ -0,0 +1,18 @@
+package com.narrativo.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+}
